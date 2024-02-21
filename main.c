bad0e28a99b2dc1064aa323ad32b6092b25bb53e
TheSailor1 <157094447+TheSailor1@users.noreply.github.com>
2024-02-21 14:12:04 +0200

    Collision wth paddle

--- main.c
+++ main.c
@@ -22,6 +22,8 @@ typedef struct Paddle {
 
 Ball ball = {0};
 Paddle paddle = {0};
+Sound fxBallHitWall;
+Sound fxBallHitPad;
 
 void drawBall(void);
 void updateBall(void);
@@ -31,8 +33,13 @@ void updatePaddle(void);
 int main(void)
 {
 	InitWindow(500, 500, "PONG");
+	InitAudioDevice();
+
 	SetWindowMonitor(1); //TEMP!!!!!
-	
+
+	fxBallHitWall = LoadSound("assets/sounds/ball_hit_wall.wav"); 
+	fxBallHitPad = LoadSound("assets/sounds/ball_hit_pad.wav"); 
+
 	SetTargetFPS(120);
 
 	ball.velocityX = 300;
@@ -45,6 +52,7 @@ int main(void)
 	paddle.speed = 500;
 	paddle.friction = 1.2;
 
+
 	while (!WindowShouldClose())
 	{
 		updateBall();
@@ -54,11 +62,12 @@ int main(void)
 		ClearBackground(RAYWHITE);
 		drawBall();
 		drawPaddle();
-
-		DrawText(TextFormat("%d", ball.velocityX), 10, 30, 20, BLACK);
-
 		EndDrawing();
 	}
+
+	UnloadSound(fxBallHitWall);
+	UnloadSound(fxBallHitPad);
+	CloseAudioDevice();
 
 	CloseWindow();
 	return 0;
@@ -71,26 +80,39 @@ void drawBall(void)
 
 void updateBall(void)
 {
+	bool collision = false;
 
 	if (ball.rec.x < 0)
 	{
+		PlaySound(fxBallHitWall);
 		ball.rec.x = 0;
 		ball.velocityX = -ball.velocityX;
 	}
 	if (ball.rec.x + ball.size > WINDOW_WIDTH)
 	{
+		PlaySound(fxBallHitWall);
 		ball.rec.x = WINDOW_WIDTH - ball.size;
 		ball.velocityX = -ball.velocityX;
 	}
-
 	if (ball.rec.y < 0)
 	{
+		PlaySound(fxBallHitWall);
 		ball.rec.y = 0;
 		ball.velocityY = -ball.velocityY;
 	}
 	if (ball.rec.y + ball.size > WINDOW_HEIGHT)
 	{
+		PlaySound(fxBallHitWall);
 		ball.rec.y = WINDOW_HEIGHT - ball.size;
+		ball.velocityY = -ball.velocityY;
+	}
+
+	// Collision with paddle
+	collision = CheckCollisionRecs(ball.rec, paddle.rec);
+	if (collision == true)
+	{
+		PlaySound(fxBallHitPad);
+		ball.rec.y = paddle.rec.y - ball.size;
 		ball.velocityY = -ball.velocityY;
 	}
 
@@ -114,6 +136,16 @@ void updatePaddle(void)
 	if (IsKeyDown(KEY_RIGHT))
 	{
 		paddle.velocityX = paddle.speed * GetFrameTime();
+	}
+
+	// Check boundaries
+	if (paddle.rec.x < 0)
+	{
+		paddle.rec.x = 0;
+	}
+	if (paddle.rec.x > WINDOW_WIDTH - paddle.rec.width)
+	{
+		paddle.rec.x = WINDOW_WIDTH - paddle.rec.width;
 	}
 
 	// Add friction to the paddle
